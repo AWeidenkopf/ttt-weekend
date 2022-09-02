8505f0d729551a753cc1540b193d8b2592f2c7ca
AWeidenkopf <oliveira.anacalado@gmail.com>
2022-09-02 18:10:03 -0400

    add margin to the board

--- js/app.js
+++ js/app.js
@@ -92,3 +92,14 @@ function getWinner() {
   }
 }
 
+// to make a computer turn 
+// create a fn that get a random num within the range of my board
+//check if that board[ranNum] is already taken
+//if not, assign e "o" to the board[ranNum]
+// if it is I need to get a diferent number
+//maybe store the numbers taken and exclude them from the search?
+
+//if the turn is -1 then is computers turn and I would need to call the 
+// computer turn fn
+
+// now I need to flip the turn again so it's player's one turn
